907e710a42da890b31ce4762ab9e3274b98cfd04
Yan Nalon <havercraft1@gmail.com>
2023-01-26 00:52:03 -0300

    Removed any vestiges of "Sabotage" related descriptions

--- src/oldschoolproject/Main.java
+++ src/oldschoolproject/Main.java
@@ -3,16 +3,22 @@ package oldschoolproject;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import oldschoolproject.Commands.Builder.CommandFramework;
 import oldschoolproject.Managers.GameManager;
 import oldschoolproject.Managers.GameType;
 
 public class Main extends JavaPlugin {
+	
+	CommandFramework cm;
 	
 	public static Main getInstance() {
 		return getPlugin(Main.class);
 	}
 	
 	public void onEnable() {
+		
+		cm = new CommandFramework(this);
+		cm.registerCommands(this);
 		
 		Bukkit.getConsoleSender().sendMessage("[OldSchool] Plugin loaded");
 	}
